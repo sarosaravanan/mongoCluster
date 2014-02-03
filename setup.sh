008590edb9e5ac5f4f9a08ca3f9baaf0d5a9df91
pmone <paragmone@gmail.com>
2014-02-02 18:36:40 -0800

    setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#Create folders for replica sets
+#========================================
+mkdir	a0	#primary on shard one
+mkdir	a1	#secondary on shard one
+mkdir 	a2	#secondary on shard one
+#========================================
+mkdir	b0	#primary on shard two
+mkdir	b1	#secondary on shard two
+mkdir	b2	#secondary on shard two
+#========================================
+#folders for config server
+mkdir	conf0
+mkdir	conf1
+mkdir	conf2
+#========================================
